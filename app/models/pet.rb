2568c3803ac9b877edad493fa428ea3d260fce95
Maria Camila Ruiz Abondano <mumi_ruiz@live.com>
2014-10-01 18:45:00 -0500

    S3 config

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,4 +4,9 @@ class Pet < ActiveRecord::Base
 
   has_attached_file :photo, :styles => { :large => "708x671#", :medium => "320x290#", :small => "288x268#", :thumb => "160x160#" }, :default_url => "/images/missing.png"
   validates_attachment_content_type :photo, :presence => true, :size => { :in => 0..10.megabytes }, :content_type => /\Aimage\/.*\Z/
+
+has_attached_file :photo,:default_url => "adoptame.amazonaws.com",
+                  :storage => :s3,
+                  :bucket => 'adoptame'
+
 end
