7469ae8130667f078a92a288f484a34efeb31141
Maria Camila Ruiz Abondano <mumi_ruiz@live.com>
2014-10-04 09:00:11 -0500

    migration for nit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141003012242) do
+ActiveRecord::Schema.define(version: 20141004061110) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20141003012242) do
     t.string   "logo_content_type"
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
+    t.integer  "nit"
   end
 
   create_table "pets", force: true do |t|
