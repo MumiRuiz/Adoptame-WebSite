e4b7c5118e57a00e433028722722ec56bb036331
Maria Camila Ruiz Abondano <mumi_ruiz@live.com>
2014-10-04 09:37:17 -0500

    correct line2 on model institution

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,5 +1,9 @@
 class Institution < ActiveRecord::Base
-	 has_attached_file :logo, :styles => { :large =>"708x671#" :medium => "470x470#", :thumb => "58x58#" }, 
+has_attached_file :logo, :styles => { :large => "708x671#", :medium => "470x470#", :thumb => "58x58xs#" },
+                  :storage => :s3,
+                  :bucket => 'adoptame-pets-photos'
 
-	  validates_attachment_content_type :logo, :presence => true, :size => { :in => 0..10.megabytes }, :content_type => /\Aimage\/.*\Z/
-end
+  validates_attachment_content_type :logo, :presence => true, :size => { :in => 0..10.megabytes }, :content_type => /\Aimage\/.*\Z/
+
+
+	end
