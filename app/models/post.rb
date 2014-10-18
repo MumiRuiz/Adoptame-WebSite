9ce7cf3d475dc800185535666b62302a3101882a
Maria Camila Ruiz Abondano <mumi_ruiz@live.com>
2014-10-18 12:44:23 -0500

    avatar blog

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ActiveRecord::Base
-  belongs_to :user
+
+	has_attached_file :avatar, :styles => { :large => "858x400#", :medium => "320x290#", :mediumsmall => "259x159#",  :small => "288x268#", :thumb => "160x160#" },
+                  :storage => :s3,
+                  :bucket => 'adoptame-pets-photos'
+    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  	belongs_to :user
 end
