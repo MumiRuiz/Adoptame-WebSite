ab4bc413cef589e2d0db2b87794a10544dc2ebf6
Maria Camila Ruiz Abondano <mumi_ruiz@live.com>
2014-10-22 18:21:47 -0500

    Search with selects

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -6,5 +6,6 @@ has_attached_file :logo, :styles => { :large => "708x671#", :medium => "470x470#
   validates_attachment_content_type :logo, :presence => true, :size => { :in => 0..10.megabytes }, :content_type => /\Aimage\/.*\Z/
 	acts_as_taggable
 
-
+has_many :pets
+validates_associated :pets
 	end
