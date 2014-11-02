22a3eed72e714930be7270a515f8a8038fcc4ce5
Jorge Madrid <jorjilio@hotmail.com>
2014-11-02 14:18:13 -0500

    mailers configurados en desarrollo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141031020639) do
+ActiveRecord::Schema.define(version: 20141102180048) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -152,8 +152,13 @@ ActiveRecord::Schema.define(version: 20141031020639) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "city"
+    t.string   "unconfirmed_email"
+    t.string   "confirmation_token"
+    t.string   "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
